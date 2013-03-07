b272987837a9b1f5848a62e2ea1f537371b6b9bf
Brayden Cleary <brayden.cleary@gmail.com>
2013-03-07 15:53:46 -0800

    Add 'name' and 'age' methods to student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,13 @@
 require_relative '../../db/config'
 
 # implement your Student model here
+class Student < ActiveRecord::Base
+  
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    (Time.now.utc.to_date - birthday).to_i / 365
+  end
+end
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -5,5 +5,52 @@ require_relative '../config'
 class CreateStudents < ActiveRecord::Migration
   def change
     # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :students do |t|
+      t.string  :first_name
+      t.string  :last_name
+      t.string  :gender
+      t.date    :birthday
+      t.string  :email
+      t.string  :phone
+    end
   end
 end
+
+
+
+# +------------+
+# | students   |
+# +------------+
+# | id         |
+# | first_name |
+# | last_name  |
+# | gender     |
+# | birthday   |
+# | email      |
+# | phone      |
+# +------------+
+
+
+# Notes from Park's talk with Shereef:
+
+# class Student < ActiveRecord::Base
+
+# end
+
+# # my_student = Student.new
+# # my_student.age = 12
+# my_student.first_name = "shereeef"
+# my_student.last_name = "bishay"
+
+# my_student.save!
+
+# # students
+
+# # first_name | last_name | age
+
+# module  ActiveRecord
+#   class Base
+#   end
+
+#   class Migration
+#   end
